348331d8c6d04cd44f1464c0df70bebff1959716
Felipe Moreno <felipe.b2014@gmail.com>
2020-03-13 13:50:08 -0300

    [React - Frontend Masters] - Eslint
    Adding and configuring eslint

--- frontend/adopt-me/src/App.js
+++ frontend/adopt-me/src/App.js
@@ -1,43 +1,30 @@
-const Pet = ({name, animal, breed}) => {
-    return React.createElement('div', {},[
-        React.createElement('h1', {}, name),
-        React.createElement('h2', {}, animal),
-        React.createElement('h2', {}, breed)
-    ]);
-}
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, name),
+    React.createElement('h2', {}, animal),
+    React.createElement('h2', {}, breed)
+  ]);
+};
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, 
-                {
-                    name: "Jhow Jhow",
-                    animal: "Dog",
-                    breed: "Mixed"
-                }
-            ),
-            React.createElement(Pet, 
-                {
-                    name: "Cadela",
-                    animal: "Cat",
-                    breed: "Mixed"
-                }
-            ),
-            React.createElement(Pet, 
-                {
-                    name: "Blue",
-                    animal: "Papagaio",
-                    breed: "Arara Azul"
-                }
-            ),
-        ]
-        );
-}
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, 'Adopt Me!'),
+    React.createElement(Pet, {
+      name: 'Jhow Jhow',
+      animal: 'Dog',
+      breed: 'Mixed'
+    }),
+    React.createElement(Pet, {
+      name: 'Cadela',
+      animal: 'Cat',
+      breed: 'Mixed'
+    }),
+    React.createElement(Pet, {
+      name: 'Blue',
+      animal: 'Papagaio',
+      breed: 'Arara Azul'
+    })
+  ]);
+};
 
-ReactDOM.render(
-    React.createElement(App),
-    document.getElementById('root')
-);
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
