3a8056d069cd17f576a319cab85d9b5ba6acd316
Felipe Moreno <cyberlegend48@gmail.com>
2020-03-16 16:00:46 -0300

    [React - Frontend Masters] - Adding forgotten files
    Adding unsaved files

--- frontend/adopt-me/src/App.js
+++ frontend/adopt-me/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { createElement } from 'react';
 import { render } from 'react-dom';
 
@@ -7,44 +6,23 @@ const Pet = ({ name, animal, breed }) => {
     createElement('h1', {}, name),
     createElement('h2', {}, animal),
     createElement('h2', {}, breed)
-=======
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, name),
-    React.createElement('h2', {}, animal),
-    React.createElement('h2', {}, breed)
->>>>>>> 9b8dfbdfdde0b9ee9c0cba3fb55ce0733ca709d5
   ]);
 };
 
 const App = () => {
-<<<<<<< HEAD
   return createElement('div', {}, [
     createElement('h1', {}, 'Adopt Me!'),
     createElement(Pet, {
-=======
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Adopt Me!'),
-    React.createElement(Pet, {
->>>>>>> 9b8dfbdfdde0b9ee9c0cba3fb55ce0733ca709d5
       name: 'Jhow Jhow',
       animal: 'Dog',
       breed: 'Mixed'
     }),
-<<<<<<< HEAD
     createElement(Pet, {
-=======
-    React.createElement(Pet, {
->>>>>>> 9b8dfbdfdde0b9ee9c0cba3fb55ce0733ca709d5
       name: 'Cadela',
       animal: 'Cat',
       breed: 'Mixed'
     }),
-<<<<<<< HEAD
     createElement(Pet, {
-=======
-    React.createElement(Pet, {
->>>>>>> 9b8dfbdfdde0b9ee9c0cba3fb55ce0733ca709d5
       name: 'Blue',
       animal: 'Papagaio',
       breed: 'Arara Azul'
@@ -52,8 +30,4 @@ const App = () => {
   ]);
 };
 
-<<<<<<< HEAD
 render(createElement(App), document.getElementById('root'));
-=======
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
->>>>>>> 9b8dfbdfdde0b9ee9c0cba3fb55ce0733ca709d5
