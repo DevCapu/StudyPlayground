896708309f5158b3d8b7102d8c766ac5e630fcfb
Felipe Moreno <cyberlegend48@gmail.com>
2020-03-27 23:51:06 -0300

    [React - Frontend Masters] - Converting to JSX
    Converting my javascript into JSX sintax

--- frontend/adopt-me/src/Pet.js
+++ frontend/adopt-me/src/Pet.js
@@ -1,9 +1,11 @@
-import { createElement } from 'react';
+import React from 'react';
 
 export default function Pet({ name, animal, breed }) {
-  return createElement('div', {}, [
-    createElement('h1', {}, name),
-    createElement('h2', {}, animal),
-    createElement('h2', {}, breed)
-  ]);
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
 }
