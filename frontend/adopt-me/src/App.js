ffc29d92b76a64087a80dde5dc42860df5defc6b
Felipe Moreno <cyberlegend48@gmail.com>
2020-03-28 00:36:56 -0300

    [React Frontend Masters] - Creating Search Component
    Creating a search component

--- frontend/adopt-me/src/App.js
+++ frontend/adopt-me/src/App.js
@@ -1,33 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 const App = () => {
-  // return createElement('div', {}, [
-  //   createElement('h1', {}, 'Adopt Me!'),
-  //   createElement(Pet, {
-  //     name: 'Jhow Jhow',
-  //     animal: 'Dog',
-  //     breed: 'Mixed'
-  //   }),
-  //   createElement(Pet, {
-  //     name: 'Cadela',
-  //     animal: 'Cat',
-  //     breed: 'Mixed'
-  //   }),
-  //   createElement(Pet, {
-  //     name: 'Blue',
-  //     animal: 'Papagaio',
-  //     breed: 'Arara Azul'
-  //   })
-  // ]);
-
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name={'Jhow'} animal={'Dog'} breed={'Mixed'} />
-      <Pet name={'Cadela'} animal={'Cat'} breed={'Mixed'} />
-      <Pet name={'Blue'} animal={'Papagaio'} breed={'Arara Azul'} />
+      <SearchParams />
     </div>
   );
 };
