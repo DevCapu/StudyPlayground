78e29abefa292463afdfbd91972f538fdc7a9633
Felipe Moreno <cyberlegend48@gmail.com>
2020-03-28 00:22:10 -0300

    Composite Components
    Using components inside other components

--- frontend/adopt-me/src/App.js
+++ frontend/adopt-me/src/App.js
@@ -1,26 +1,35 @@
-import { createElement } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import Pet from './Pet';
 
 const App = () => {
-  return createElement('div', {}, [
-    createElement('h1', {}, 'Adopt Me!'),
-    createElement(Pet, {
-      name: 'Jhow Jhow',
-      animal: 'Dog',
-      breed: 'Mixed'
-    }),
-    createElement(Pet, {
-      name: 'Cadela',
-      animal: 'Cat',
-      breed: 'Mixed'
-    }),
-    createElement(Pet, {
-      name: 'Blue',
-      animal: 'Papagaio',
-      breed: 'Arara Azul'
-    })
-  ]);
+  // return createElement('div', {}, [
+  //   createElement('h1', {}, 'Adopt Me!'),
+  //   createElement(Pet, {
+  //     name: 'Jhow Jhow',
+  //     animal: 'Dog',
+  //     breed: 'Mixed'
+  //   }),
+  //   createElement(Pet, {
+  //     name: 'Cadela',
+  //     animal: 'Cat',
+  //     breed: 'Mixed'
+  //   }),
+  //   createElement(Pet, {
+  //     name: 'Blue',
+  //     animal: 'Papagaio',
+  //     breed: 'Arara Azul'
+  //   })
+  // ]);
+
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name={'Jhow'} animal={'Dog'} breed={'Mixed'} />
+      <Pet name={'Cadela'} animal={'Cat'} breed={'Mixed'} />
+      <Pet name={'Blue'} animal={'Papagaio'} breed={'Arara Azul'} />
+    </div>
+  );
 };
 
-render(createElement(App), document.getElementById('root'));
+render(<App />, document.getElementById('root'));
