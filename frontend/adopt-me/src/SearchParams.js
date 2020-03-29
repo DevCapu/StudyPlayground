08c8ac539e95dc9a2ac9d87f06bb5719de63fc5e
Felipe Moreno Borges <cyberlegend48@gmail.com>
2020-03-29 03:36:15 -0300

    [React - Frontend Masters] - Animal breed and FrontendMasters API
    Adding a breed select and installing frontendmasters API to request the animals

--- frontend/adopt-me/src/SearchParams.js
+++ frontend/adopt-me/src/SearchParams.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import { ANIMALS } from '@frontendmasters/pet';
 
 const SearchParams = () => {
   const [location, setLocation] = useState('Seattle, WA');
+  const [animal, setAnimal] = useState('dog');
+  const [breed, setBreed] = useState('');
+  const [breeds, setBreeds] = useState([]);
 
   return (
     <div className="search-params">
@@ -14,6 +18,38 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={e => setLocation(e.target.value)}
           />
+        </label>
+        <label>
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onBlur={e => setAnimal(e.target.value)}
+            onChange={e => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          <select
+            id="breed"
+            value={breed}
+            onChange={e => setBreed(e.target.value)}
+            onBlur={e => setBreed(e.target.value)}
+            disabled={breeds.length === 0}
+          >
+            <option>All</option>
+            {breeds.map(breedString => (
+              <option key={breedString} value={breedString}>
+                {breedString}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
