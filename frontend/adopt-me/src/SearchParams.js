6220ef778e0f83f90358fc8bc2a7e0aa3d226986
Felipe Moreno Borges <cyberlegend48@gmail.com>
2020-04-01 00:22:54 -0300

    [React - Frontend Masters] - Asynchornus API Request
    Async funcionality to make a request to tge API, and configuring the package.json to target mordern browsers

--- frontend/adopt-me/src/SearchParams.js
+++ frontend/adopt-me/src/SearchParams.js
@@ -7,6 +7,17 @@ const SearchParams = () => {
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown('Animal', 'dog', ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown('Breed', '', breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+
+    setPets(animals || []);
+  }
 
   useEffect(() => {
     setBreeds([]);
@@ -20,7 +31,12 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           location
           <input
